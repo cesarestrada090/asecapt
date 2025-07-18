118e9a380c2503668eabaa7ef05aa245212ff6e8
Estrada <cesar.estrada@sap.com>
2025-07-18 01:49:12 -0500

    Fix for landing page

--- asecapt-final-clean/src/app/app.routes.ts
+++ asecapt-final-clean/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { CoursesGridComponent } from './components/courses-grid/courses-grid.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 export const routes: Routes = [
+  { path: '', component: LandingPageComponent },
   { path: 'courses-grid', component: CoursesGridComponent },
-  { path: '', redirectTo: '', pathMatch: 'full' }
+  { path: '**', redirectTo: '' }
 ]; 
