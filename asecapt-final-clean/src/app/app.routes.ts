59cfd88847598f2e30d91e0018169784893b46a8
Estrada <cesar.estrada@sap.com>
2025-07-26 19:48:51 -0500

    Remove unnecesary sections

--- asecapt-final-clean/src/app/app.routes.ts
+++ asecapt-final-clean/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { CoursesGridComponent } from './components/courses-grid/courses-grid.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { AboutComponent } from './components/about/about.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'courses-grid', component: CoursesGridComponent },
+  { path: 'about', component: AboutComponent },
   { path: '**', redirectTo: '' }
 ]; 
---
+++ asecapt-final-clean/src/app/components/about/about.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [RouterLink, CommonModule],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.css'
+})
+export class AboutComponent {
+
+} 
