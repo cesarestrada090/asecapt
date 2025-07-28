f6b0fdd0a1fb9e4afcd9bf22a7056b6402e1ca92
Estrada <cesar.estrada@sap.com>
2025-07-28 13:14:44 -0500

    add aula desktop

--- asecapt-final-clean/src/app/app.component.ts
+++ asecapt-final-clean/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
@@ -14,5 +14,24 @@ export class AppComponent {
 
   toggleMobileMenu() {
     this.isMobileMenuOpen = !this.isMobileMenuOpen;
+    console.log('Menu toggled:', this.isMobileMenuOpen); // Debug
+  }
+
+  closeMobileMenu() {
+    this.isMobileMenuOpen = false;
+    console.log('Menu closed'); // Debug
+  }
+
+  // Cerrar menú al hacer click en cualquier parte de la página
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: Event) {
+    const target = event.target as HTMLElement;
+    
+    // Si el menú está abierto y el click NO es en el botón del menú ni en el menú mismo
+    if (this.isMobileMenuOpen && 
+        !target.closest('.navbar-toggler') && 
+        !target.closest('#nav')) {
+      this.isMobileMenuOpen = false;
+    }
   }
 }
